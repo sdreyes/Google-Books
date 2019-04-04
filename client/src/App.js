2f454ecef7c731009ca4eaa619673d3cef79f64d
sdreyes <sheshechan90@gmail.com>
2019-04-04 17:55:45 -0400

    Import nav and place it above the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Nav from "./components/Nav";
 import Search from "./pages/Search";
 import Saved from "./pages/Saved";
 import NoMatch from "./pages/NoMatch";
@@ -8,6 +9,7 @@ function App() {
   return (
     <Router>
       <div>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Search} />
           <Route exact path="/saved" component={Saved} />
