720a62ad05a87e9c91770cceea7f8b8fed2bebf8
sdreyes <sheshechan90@gmail.com>
2019-04-04 17:56:30 -0400

    Create and display the title and form

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,9 +1,30 @@
 import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
 
 class Search extends Component {
     render() {
         return (
-            <div>Search page</div>
+            <Container>
+                <Row>
+                    <div className="col rounded text-center bg-info m-4 p-4">
+                        <h1>React Google Books Search</h1>
+                        <h4>Search For and Save Books of Interest</h4>
+                    </div>
+                </Row>
+                <Row>
+                    <div className="col rounded bg-light mb-4 mr-4 ml-4 mt-0 p-4">
+                        <h4>Book Search</h4>
+                        <form>
+                            <div class="form-group">
+                                <label for="book-input">Book</label>
+                                <input type="text" class="form-control" id="book-input" placeholder="Harry Potter" />
+                            </div>
+                            <button type="submit" class="btn btn-primary">Search</button>
+                            <img className="ml-3" src="https://books.google.com/googlebooks/images/poweredby.png" alt="Powered by Google" />
+                        </form>
+                    </div>
+                </Row>
+            </Container>
         )
     }
 }
