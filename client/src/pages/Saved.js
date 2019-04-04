cf2926c837a1323a4fb2f675dc686e119df0225a
sdreyes <sheshechan90@gmail.com>
2019-04-04 16:10:10 -0400

    Initialize site pages

---
+++ client/src/pages/Saved.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Saved extends Component {
+    render() {
+        return (
+            <div>Saved page</div>
+        )
+    }
+}
+
+export default Saved;
