3a556645a475b847f35b02e10a281b1c4a0f2e1f
sdreyes <sheshechan90@gmail.com>
2019-04-08 16:33:38 -0400

    Create API routes for saved books using express in conjunction with the controller functions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const router = require("express").Router();
+const booksController = require("../controllers/booksController");
 
 router.get("/books", (req, res) => {
     axios
@@ -7,5 +8,12 @@ router.get("/books", (req, res) => {
         .then(({ data: { items } }) => res.json(items))
         .catch(err => res.status(422).json(err));
 });
+
+router.route("/savedBooks")
+    .get(booksController.findAll)
+    .post(booksController.create);
+
+router.route("/savedBooks/:id")
+    .delete(booksController.remove);
   
 module.exports = router;
