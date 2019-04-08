82b5da6269ca561f8b087f65cf9ab1c6e1015e62
sdreyes <sheshechan90@gmail.com>
2019-04-08 16:29:08 -0400

    Join author array using reduce method if the length of the array is greater than 1. Check if the user has already saved the book to determine if the 'delete' or 'save' button should be rendered

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -8,20 +8,27 @@ export function BookList({ children }) {
 };
 
 export function BookListItem({
+    id,
     title,
     authors,
     description,
     thumbnail,
-    href
+    href,
+    clickEvent,
+    saved
 }) {
     return (
         <li className="list-group-item">
-            <button className="float-right btn btn-success">Save</button>
+            {!saved ? (
+            <button className="float-right btn btn-success" onClick={event => clickEvent(event, title, authors, description, href, thumbnail)}>Save</button>
+            ) : (
+            <button className="float-right btn btn-danger" onClick={event => clickEvent(event, id)}>Delete</button>
+            )}
             <a className="float-right btn btn-primary ml-2 mr-2" href={href} target="_blank" rel="noopener noreferrer">View</a>
             <h4>{title}</h4>
-            <h5>by {authors}</h5>
+            <h5>by {authors.length > 1 ? (authors.reduce((prev, curr) => [prev, ", ", curr])) : authors[0]}</h5>
             <Row>
-                <div class="col-auto">
+                <div className="col-auto">
                     <img src={thumbnail} alt={title} />
                 </div>
                 <Col>
