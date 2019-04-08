5bf0506010a523c307c75c037669afffe9a7ac67
sdreyes <sheshechan90@gmail.com>
2019-04-08 01:11:51 -0400

    Add functions for get, post, and delete requests from Mongoose

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,5 +3,14 @@ import axios from "axios";
 export default {
   getBooks: function(query) {
     return axios.get("/api/books", { params: { q: query } });
+  },
+  getSavedBooks: function() {
+      return axios.get("/api/saved");
+  },
+  deleteSavedBook: function(id) {
+      return axios.delete(`/api/saved/${id}`);
+  },
+  saveBook: function(bookData) {
+      return axios.post(`/api/saved${bookData}`);
   }
 };
