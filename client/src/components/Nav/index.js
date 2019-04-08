6b5c8fa42c595c28d043d6dee78269b5b07dadd4
sdreyes <sheshechan90@gmail.com>
2019-04-07 23:41:37 -0400

    Change class to className per React documentation

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,19 +2,19 @@ import React from "react";
 
 function Nav() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/">React Book Search</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">React Book Search</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/">Search</a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Search</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/saved">Saved</a>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/saved">Saved</a>
                     </li>
                 </ul>
             </div>
