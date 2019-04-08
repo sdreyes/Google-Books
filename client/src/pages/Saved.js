ee82ab69ee73a0f2debf1afc6d81c6f68fb9390a
sdreyes <sheshechan90@gmail.com>
2019-04-08 01:10:04 -0400

    Format saved page to the same layout as the search page

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Button from "../components/Button";
+import { BookList, BookListItem } from "../components/BookList";
 
 class Saved extends Component {
+
+    componentDidMount() {
+        // this.loadSavedBooks();
+    }
+
     render() {
         return (
-            <div>Saved page</div>
+            <Container>
+                <Row>
+                    <div className="col rounded text-center bg-success mt-4 p-4">
+                        <h1>Saved React Google Books</h1>
+                        <h4>Your Favorites</h4>
+                    </div>
+                </Row>
+            </Container>
         )
     }
 }
