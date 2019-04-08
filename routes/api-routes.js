2ff415cc6219e9918b691054851b9bb91a99270d
sdreyes <sheshechan90@gmail.com>
2019-04-07 23:44:18 -0400

    Remove unnecessary console log

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,6 @@ const axios = require("axios");
 const router = require("express").Router();
 
 router.get("/books", (req, res) => {
-    console.log(req.query.q);
     axios
         .get("https://www.googleapis.com/books/v1/volumes", { params: req.query })
         .then(({ data: { items } }) => res.json(items))
