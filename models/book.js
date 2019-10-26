b9b74be62307d6f81925fb7d584a88f7b21db432
sdreyes <sheshechan90@gmail.com>
2019-10-26 10:52:03 -0400

    Add required googleId field

--- models/book.js
+++ models/book.js
@@ -2,27 +2,31 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    authors: {
-        type: Array,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    href: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    thumbnail: {
-        type: String,
-        required: true
-    }
+  googleId: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: Array,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  href: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  thumbnail: {
+    type: String,
+    required: true
+  }
 });
 
 const Book = mongoose.model("Book", bookSchema);
