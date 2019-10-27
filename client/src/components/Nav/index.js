fe0af22a4b4e3a175810b1ce047fa6a60f5e4490
sdreyes <sheshechan90@gmail.com>
2019-10-27 10:32:11 -0400

    Remove hamburger button from navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,22 +2,18 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
       <a className="navbar-brand" href="/">React Book Search</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <a className="nav-link" href="/">Search</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/saved">Saved</a>
-          </li>
-        </ul>
-      </div>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="/">Search</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/saved">Saved</a>
+        </li>
+      </ul>
+
     </nav>
   )
 };
